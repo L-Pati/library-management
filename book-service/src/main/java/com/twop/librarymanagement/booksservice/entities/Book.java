6ee4232dc25d2c39e02823d398009a744a01ac8e
L-Pati <lasickels@gmail.com>
2020-08-10 21:47:02 -0500

    books : adds book class

---
+++ book-service/src/main/java/com/twop/librarymanagement/booksservice/entities/Book.java
@@ -0,0 +1,25 @@
+package com.twop.librarymanagement.booksservice.entities;
+
+public class Book {
+    private String title;
+    private String author;
+    private int id;
+
+    public Book(String title, String author, int id) {
+        this.title = title;
+        this.author = author;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getID() {
+        return this.id;
+    }
+}
